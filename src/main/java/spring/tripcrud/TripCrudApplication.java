80869414c34781c6c30b91ac1883879f58bd6848
dhalilaj <halilajdiona@gmail.com>
2023-02-23 18:34:16 +0100

    init

---
+++ src/main/java/spring/tripcrud/TripCrudApplication.java
@@ -0,0 +1,13 @@
+package spring.tripcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/tripcrud/TripCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.tripcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
